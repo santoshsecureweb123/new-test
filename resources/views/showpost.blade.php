7c786f7866811ceb5f5785f822d56e4be20ccfe5
santoshsecureweb123 <santoshsecureweb@gmail.com>
2019-10-15 16:11:30 +0530

    Push Branches2

--- resources/views/showpost.blade.php
+++ resources/views/showpost.blade.php
@@ -24,11 +24,6 @@
         <div class="col-md-3"> </div>
     </div>
 	
-	<div class="row">        
-	<div class="col-md-3"></div>
-	<div class="col-md-6"></div>
-	<div class="col-md-3"> </div>
-    </div>
 @endsection
 
 
