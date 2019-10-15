c1a5f99003682665349cc5ad885da22c160cf7d4
santoshsecureweb123 <santoshsecureweb@gmail.com>
2019-10-15 15:45:33 +0530

    push

--- resources/views/showpost.blade.php
+++ resources/views/showpost.blade.php
@@ -17,5 +17,12 @@
     </div>
     <?php } ?>
 </div>
-
+ 
 @endsection
+
+
+    <div class="row">        
+        <div class="col-md-3"></div>
+        <div class="col-md-6"></div>
+        <div class="col-md-3"> </div>
+    </div>
